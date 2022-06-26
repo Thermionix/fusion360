516d37215d9073d4ddafb77a3d46a304db4e0306
thermionix <thermionix@gmail.com>
2022-06-26 17:06:04 +1000

    updated to fix installation issue

---
+++ build_test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# snapcraft clean
+# snapcraft
+
+sudo snap remove --purge fusion360
+sudo snap install fusion360_0.8_amd64.snap --dangerous --devmode
+
+mkdir -p "${HOME}/snap/fusion360/common/.cache/"
+cp -R "${HOME}/snap/.cache/winetricks/" "${HOME}/snap/fusion360/common/.cache/"
+#export XDG_CACHE_HOME="/tmp/.cache"
+
+#export SOMMELIER_DEBUG=1 
+#export WINEDEBUG=err+all
+
+fusion360
