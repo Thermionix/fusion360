06488ea9a9539dd5df1f5df8b83eb2f3317a82c5
thermionix <thermionix@gmail.com>
2022-06-28 16:25:28 +1000

    moved dxvk-setup and updater into snap bin/

--- build_test.sh
+++ build_test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# snapcraft clean
-# snapcraft
+snapcraft clean content
+snapcraft
 
 sudo snap remove --purge fusion360
-sudo snap install fusion360_0.9_amd64.snap --dangerous --devmode
+sudo snap install fusion360_1.0_amd64.snap --dangerous --devmode
 
 mkdir -p "${HOME}/snap/fusion360/common/.cache/"
 cp -R "${HOME}/snap/.cache/winetricks/" "${HOME}/snap/fusion360/common/.cache/"
@@ -13,4 +13,6 @@ cp -R "${HOME}/snap/.cache/winetricks/" "${HOME}/snap/fusion360/common/.cache/"
 #export SOMMELIER_DEBUG=1 
 #export WINEDEBUG=err+all
 
-fusion360
+# snap run --shell fusion360 -c 'env' | grep XDG
+# fusion360.dxvk-setup
+# fusion360.updater
