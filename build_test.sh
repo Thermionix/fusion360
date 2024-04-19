f9440395bd92a8657d183b7165febad44a71a5b3
Thermionix <michael.bond@posteo.de>
2024-04-20 09:02:46 +1000

    refactor to core22

--- build_test.sh
+++ build_test.sh
@@ -1,13 +1,32 @@
 #!/bin/sh
 
+##arch-linux snapcraft setup##
+# yay -S snapd
+# sudo systemctl enable --now snapd.socket
+# sudo ln -s /var/lib/snapd/snap /snap
+# sudo snap install snapcraft --classic
+# sudo snap install lxd --classic
+# sudo usermod -aG lxd `whoami`
+#### reboot to group change, don't build using sudo
+# lxd init --auto
+# snapcraft --verbose  # in fusion360 snap dir
+
+# wine-platform repo list
+# https://github.com/mmtrt?tab=repositories&q=wine-platform&type=&language=&sort=
+
+rm fusion360_1.5_amd64.snap
+
 snapcraft clean content
-snapcraft
+snapcraft --verbose
 
 sudo snap remove --purge fusion360
-sudo snap install fusion360_1.1_amd64.snap --dangerous --devmode
+sudo snap install fusion360_1.5_amd64.snap --dangerous --devmode
 
-mkdir -p "${HOME}/snap/fusion360/common/.cache/"
-cp -R "${HOME}/snap/.cache/winetricks/" "${HOME}/snap/fusion360/common/.cache/"
+mkdir -p "${HOME}/snap/fusion360/common/.cache/" &&
+cp -R "${HOME}/snap/winetricks/" "${HOME}/snap/fusion360/common/.cache/"
+
+export WINEDEBUG=err+all
+
 #export XDG_CACHE_HOME="/tmp/.cache"
 
 #export SOMMELIER_DEBUG=1 
